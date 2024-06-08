ab9d414b310a8c1082641c176635c0862e0f4414
Mandaa <99118244+rmndkyl@users.noreply.github.com>
2024-06-08 07:07:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ figlet.text('TX - Bot', {
     
     provider.once('block', (transaction) => {
     console.log("Wallet Balance Auto Sender / Address Cleaner\n");
-    console.log("- https://github.com/hanzvibes/tx-bot\n");
+    console.log("- https://github.com/rmndkyl/tx-bot\n");
     console.log("Current Network State :\n");
     console.log("Block Number : ",transaction);
     });
